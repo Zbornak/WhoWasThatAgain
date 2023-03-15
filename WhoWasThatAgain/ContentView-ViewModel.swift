4dbde0e781593a285abce1de520e970beee90a54
Mark <zbornack@protonmail.com>
2023-03-15 13:37:05 +0000

    Added view models

--- WhoWasThatAgain/ContentView-ViewModel.swift
+++ WhoWasThatAgain/ContentView-ViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+extension ContentView {
+    @MainActor class ContentViewModel: ObservableObject {
+        
+    }
+}
--- WhoWasThatAgain/DetailView-ViewModel.swift
+++ WhoWasThatAgain/DetailView-ViewModel.swift
@@ -6,3 +6,9 @@
 //
 
 import Foundation
+
+extension DetailView {
+    @MainActor class DetailViewModel: ObservableObject {
+        
+    }
+}
