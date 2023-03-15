ff1d1981d53c7e4f22869dd6610b93bf0887705d
Mark <zbornack@protonmail.com>
2023-03-15 12:00:30 +0000

    Initial Commit

---
+++ WhoWasThatAgain/WhoWasThatAgainApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoWasThatAgainApp.swift
+//  WhoWasThatAgain
+//
+//  Created by Mark Strijdom on 15/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WhoWasThatAgainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
