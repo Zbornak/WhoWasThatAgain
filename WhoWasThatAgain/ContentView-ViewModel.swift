fe0a18b57d57a20a1057fd1ce89859f07d2fd3d6
Mark <zbornack@protonmail.com>
2023-03-15 12:03:03 +0000

    Created detailview and viewmodels for both views

---
+++ WhoWasThatAgain/ContentView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ContentView-ViewModel.swift
+//  WhoWasThatAgain
+//
+//  Created by Mark Strijdom on 15/03/2023.
+//
+
+import Foundation
---
+++ WhoWasThatAgain/DetailView-ViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  DetailView-ViewModel.swift
+//  WhoWasThatAgain
+//
+//  Created by Mark Strijdom on 15/03/2023.
+//
+
+import Foundation
