7dce93354941853aaf5ee6f41f705238ec66e61a
Mark <zbornack@protonmail.com>
2023-03-16 10:38:14 +0000

    minor change

--- WhoWasThatAgain/WhoWasThatAgainApp.swift
+++ WhoWasThatAgain/WhoWasThatAgainApp.swift
@@ -14,6 +14,7 @@ struct WhoWasThatAgainApp: App {
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environment(\.managedObjectContext, dataController.container.viewContext)
         }
     }
 }
