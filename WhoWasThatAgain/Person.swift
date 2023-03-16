3ff6e5d965458b5ba098571032ce132b2aa1db73
Mark <zbornack@protonmail.com>
2023-03-16 10:29:24 +0000

    changed struct to reflect data model

--- WhoWasThatAgain/Person.swift
+++ WhoWasThatAgain/Person.swift
@@ -10,11 +10,11 @@ import Foundation
 struct Person: Identifiable, Codable, Equatable {
     var id: UUID
     var name: String
-    var description: String
+    var information: String
     var role: String
     var meetingPlace: String
     
-    static let example = Person(id: UUID(), name: "Bob Zerunckle", description: "One ear, calls everyone Jim", role: "Senior Developer, Vanderlay Industries", meetingPlace: "WWDC2018")
+    static let example = Person(id: UUID(), name: "Bob Zerunckle", information: "One ear, calls everyone Jim", role: "Senior Developer, Vanderlay Industries", meetingPlace: "WWDC2018")
     
     static func ==(lhs: Person, rhs: Person) -> Bool {
         lhs.id == rhs.id
