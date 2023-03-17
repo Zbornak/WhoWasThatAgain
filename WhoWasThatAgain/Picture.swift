a9d20e03426a08566d142893ef8c957e75ef88ee
Mark <zbornack@protonmail.com>
2023-03-17 10:52:25 +0000

    Added picture struct

---
+++ WhoWasThatAgain/Picture.swift
@@ -0,0 +1,13 @@
+//
+//  Picture.swift
+//  WhoWasThatAgain
+//
+//  Created by Mark Strijdom on 17/03/2023.
+//
+
+import SwiftUI
+
+struct Picture: Identifiable {
+    var id: UUID
+    var name: String
+}
